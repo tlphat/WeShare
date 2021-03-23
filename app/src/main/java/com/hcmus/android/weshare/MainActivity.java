9cd4031f91ea31d549cdc5cbf4e5bf37e6b13ebb
Phat <tlphat18@apcs.vn>
2021-03-23 20:47:56 +0700

    Feature #7: Remember user in the next login
    - Add SharedPreferences for storing latest login info

--- app/src/main/java/com/hcmus/android/weshare/MainActivity.java
+++ app/src/main/java/com/hcmus/android/weshare/MainActivity.java
@@ -3,17 +3,17 @@ package com.hcmus.android.weshare;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseUser;
 import com.hcmus.android.weshare.viewmodel.AuthenticationViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,12 +25,43 @@ public class MainActivity extends AppCompatActivity {
 
     private AuthenticationViewModel authenticationViewModel;
 
+    /** The shared preferences are used to store latest login information */
+    private SharedPreferences latestLoginPreferences;
+    private SharedPreferences.Editor latestLoginEditor;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initComponents();
+        loginWithLatestInfo();
+    }
+
+    private void initComponents() {
+        initSharePreferences();
+        initEditTexts();
         initViewModel();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void loginWithLatestInfo() {
+        String latestEmail = latestLoginPreferences.getString("email", "");
+        String latestPassword = latestLoginPreferences.getString("password", "");
+        loginWithGivenEmailAndPassword(latestEmail, latestPassword);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void loginWithGivenEmailAndPassword(String email, String password) {
+        emailEditText.setText(email);
+        passwordEditText.setText(password);
+        loginWithEmailAndPassword(email, password);
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private void initSharePreferences() {
+        latestLoginPreferences = getSharedPreferences("login_preferences", MODE_PRIVATE);
+        latestLoginEditor = latestLoginPreferences.edit();
     }
 
     private void initViewModel() {
@@ -42,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void initComponents() {
+    private void initEditTexts() {
         emailEditText = findViewById(R.id.login_email_edit_text);
         passwordEditText = findViewById(R.id.login_password_edit_text);
     }
@@ -51,14 +82,22 @@ public class MainActivity extends AppCompatActivity {
     public void loginButtonClick(View view) {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
+        loginWithEmailAndPassword(email, password);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void loginWithEmailAndPassword(String email, String password) {
+        storeLoginInfo(email, password);
         authenticationViewModel.login(email, password);
     }
 
-    public void signUpButtonClick(View view) {
-        navigateToRegisterActivity();
+    private void storeLoginInfo(String email, String password) {
+        latestLoginEditor.putString("email", email);
+        latestLoginEditor.putString("password", password);
+        latestLoginEditor.commit();
     }
 
-    public void navigateToRegisterActivity() {
+    public void signUpButtonClick(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivityForResult(intent, REGISTER_CODE);
     }
@@ -70,9 +109,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REGISTER_CODE && resultCode == RESULT_OK) {
             String email = data.getStringExtra("email");
             String password = data.getStringExtra("password");
-            emailEditText.setText(email);
-            passwordEditText.setText(password);
-            authenticationViewModel.login(email, password);
+            loginWithGivenEmailAndPassword(email, password);
         }
     }
 }
